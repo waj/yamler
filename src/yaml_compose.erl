dd76198a5b5e8f65f4112a1a892600b181471877
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-05-08 15:35:02 -0300

    Rollback last change and add flag to make it compatible with Erlang R16 and R17

--- src/yaml_compose.erl
+++ src/yaml_compose.erl
@@ -31,7 +31,7 @@
 
 -record(state, {
 				events :: [yaml_libyaml:event()],
-				anchors :: dict:dict(),
+				anchors :: dict(),
 				schema :: atom(),
 				schema_state :: term(),
 				sort_mappings :: boolean()
